8c021c293f97c6d7b668b8bb8b664161682651a0
scyyythe <caneteangel187@gmail.com>
2024-08-18 17:18:17 +0800

    new new

---
+++ script.js
@@ -0,0 +1,11 @@
+function greet(name,lastName){
+    console.log('Hello ' + name  + lastName);
+}
+
+greet('Angel', 'Canete');
+
+function square(number){
+    return number*number;
+}
+
+console.log(square(2));
